f9096b2d1569c3e95d5a8524df16370b18ab5e1e
yuanchen <yuanchen1103@gmail.com>
2018-06-06 10:53:11 +0800

    node module

---
+++ node_modules/body-scroll-lock/lib/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _bodyScrollLock = require('./bodyScrollLock');
+
+var _bodyScrollLock2 = _interopRequireDefault(_bodyScrollLock);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _bodyScrollLock2.default;
